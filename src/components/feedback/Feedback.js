3fce70494837422ce3709e35e6d2647e9bf12e52
Sergey Donyk <doncarlos@i.ua>
2023-04-20 02:17:12 +0300

    qwerty

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
 export class Feedback extends Component {
-  static defaultProps = {
-    step: 1,
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
-
+  handleIncrementGood() {
+    console.log('i`ll by back');
+  }
+  handleIncrementNeutral() {
+    console.log('i`ll by back');
+  }
+  handleIncrementBad() {
+    console.log('i`ll by back');
+  }
   render() {
     const { step } = this.props;
 
     return (
       <div>
         <span>0</span>
-        <button type="button">Increment by {step}</button>
-        <button type="button">Decrement by {step}</button>
+        <button type="button" onClick={this.handleIncrementGood}>
+          Good {step}
+        </button>
+        <button type="button" onClick={this.handleIncrementNeutral}>
+          Neutral {step}
+        </button>
+        <button type="button" onClick={this.handleIncrementBad}>
+          Bad {step}
+        </button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Feedback step={5} />, document.getElementById('root'));
+// ReactDOM.render(<Feedback step={5} />, document.getElementById('root'));
