74b0ec9e7b59fcc0205bdf25899d4d7680cae75e
Sergey Donyk <doncarlos@i.ua>
2023-04-25 01:32:55 +0300

    Update Feedback.js

--- src/components/feedback/Feedback/Feedback.js
+++ src/components/feedback/Feedback/Feedback.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import css from './Feedback.module.css';
-console.log(css);
+
 export const Feedback = ({
   options,
   onLeaveFeedback,
